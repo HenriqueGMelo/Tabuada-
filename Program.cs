55063b4ff262407972f5e065a8b0d6d2cb85005a
Henrique Melo <Henrique-125@hotmail.com>
2022-07-18 10:17:52 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero do qual deseja obter a Tabuada: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tabuada do " + n);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                int resultado = n * i;
+                Console.WriteLine(n + " x " + i + " = " + resultado);
+
+            }
+        }
+    }
+}
